fdb13fcf0697a3bd388cc7c0010d469e35e502c8
waynemamahit <wayne.mamahit@gmail.com>
2025-10-29 09:45:42 +0700

    chore: update dependencies, wrangler configuration, and starter page

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useHead({
+  title: 'Nuxt Hono Starter Project',
+  meta: [
+    {
+      name: 'description',
+      content:
+        'A minimal starter project for building Nuxt applications with Hono.',
+    },
+  ],
+});
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
